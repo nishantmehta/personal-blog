2370efbd23050da73d922d1c892a6587ec4e10df
Nishant Mehta <nishantmehta@users.noreply.github.com>
2024-04-11 18:27:21 +0000

    added logging to webserver

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use actix_web::{web, App, HttpServer, Responder};
 use chrono::{Utc};
+use log::info;
+use log4rs;
 
 async fn index() -> impl Responder {
+    info!("accepting request on /");
     "Hello from Rust web server!"
 }
 
 async fn sysInfo() -> impl Responder {
+    info!("accepting request on /sysInfo");
     let now = Utc::now();
     format!("Hello, world! Today is {}.", now.time().to_string())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    info!("Application started");
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
