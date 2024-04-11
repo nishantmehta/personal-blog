4fe226be5dfc6d528c6af1ee3ad20d58ccd854b6
Nishant Mehta <nishantmehta@users.noreply.github.com>
2024-04-11 18:00:02 +0000

    base webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+use chrono::{Utc};
+
+async fn index() -> impl Responder {
+    "Hello from Rust web server!"
+}
+
+async fn sysInfo() -> impl Responder {
+    let now = Utc::now();
+    format!("Hello, world! Today is {}.", now.time().to_string())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/sysInfo", web::get().to(sysInfo))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
